66cbec996bf1ecd8c03b14d33197a42fd491d225
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-03-14 17:12:07 -0700

    add social icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import Logo from '../assets/reactlogoWhite.png';
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
+import Logo from '../assets/reactlogo.png';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -36,8 +39,29 @@ const Navbar = () => {
     </ul>
 
     { /* social icons */ } 
-    <div></div>
-    
+    <div className='flex fixed flex-col top-[35%] left-0'></div>
+    <ul>
+      <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+        <a className='flex justify-between items-center w-full text-white' href="/">
+          Linkedin <FaLinkedin size={30}/>
+        </a>
+      </li>
+      <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+        <a className='flex justify-between items-center w-full text-white' href="/">
+          GitHub<FaGithub size={30}/>
+        </a>
+      </li>
+      <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+        <a className='flex justify-between items-center w-full text-white' href="/">
+          Email<HiOutlineMail size={30}/>
+        </a>
+      </li>
+      <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+        <a className='flex justify-between items-center w-full text-white' href="/">
+          Resume <BsFillPersonLinesFill size={30}/>
+        </a>
+      </li>
+    </ul>
 
     </div>
   )
