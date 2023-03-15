bfe445df88a8f2025fd2d3e9b03467c773861cdf
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-03-14 17:24:45 -0700

    updated Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,22 @@ import { HiArrowNarrowRight } from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
-      
+    <div name="home" className='w-full h-screen bg-[#0a192f]'>
+
+    {/* container */} 
+    <div className='max-w-[800px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-300'>Hello! My name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Lauren Genzone</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div>
+            <button>View Work<HiArrowNarrowRight /></button>
+        </div>
+
+    </div>
+
+
+     
     </div>
   )
 }
