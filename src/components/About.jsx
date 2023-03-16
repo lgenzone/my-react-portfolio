5d3605c7f134dcf48f60a0bd461fdfcabe0b843c
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-03-15 21:19:29 -0700

    updated About Me

---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const About = () => {
+    return (
+      <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+          <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+            <div className='sm:text-right pb-8 pl-4'>
+              <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+                About Me
+              </p>
+            </div>
+            <div></div>
+            </div>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+              <div className='sm:text-right text-4xl font-bold'>
+                <p>Hi, I'm Lauren Genzone. Say something about yourself here.</p>
+              </div>
+              <div>
+                <p> Talk about yourself as a coder and what you're passionate about. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>  
+              </div>
+            </div>
+        </div>
+      </div>
+    );
+  };
+
+export default About
+
