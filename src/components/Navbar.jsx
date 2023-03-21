f5a1d910a60f01bf10de97f6f5edcbff0d621728
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-03-20 22:43:38 -0700

    fixed navbar - highlight current page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-
+import { useLocation } from 'react-router-dom';
 
 const colors = {
   "Background": "#020100",
@@ -9,8 +9,9 @@ const colors = {
 };
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
-  const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+  const location = useLocation();
 
   return (
     <div
@@ -20,19 +21,19 @@ const Navbar = () => {
       {/* menu */}
       <ul className="hidden md:flex">
         <li>
-          <a href='/'>Home</a>
+          <a href='/' style={{ color: location.pathname === '/' ? colors.Accent : colors.Text }}>Home</a>
         </li>
         <li>
-          <a href='/about'>About</a>
+          <a href='/about' style={{ color: location.pathname === '/about' ? colors.Accent : colors.Text }}>About</a>
         </li>
         <li>
-          <a href='/projects'>Projects</a>
+          <a href='/projects' style={{ color: location.pathname === '/projects' ? colors.Accent : colors.Text }}>Projects</a>
         </li>
         <li>
-          <a href='/resume'>Resume</a>
+          <a href='/resume' style={{ color: location.pathname === '/resume' ? colors.Accent : colors.Text }}>Resume</a>
         </li>
         <li>
-          <a href='/contact'>Contact</a>
+          <a href='/contact' style={{ color: location.pathname === '/contact' ? colors.Accent : colors.Text }}>Contact</a>
         </li>
       </ul>
 
@@ -45,19 +46,19 @@ const Navbar = () => {
       <ul
         className={`absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center ${!nav ? "hidden" : ""}`} style={{backgroundColor: colors.Background}}>
         <li className="py-6 text-4xl">
-          <a href='/'>Home</a>
+          <a href='/' style={{ color: location.pathname === '/' ? colors.Accent : colors.Text }}>Home</a>
         </li>
         <li className="py-6 text-4xl">
-        <a href='/about'>About</a>
+        <a href='/about' style={{ color: location.pathname === '/about' ? colors.Accent : colors.Text }}>About</a>
         </li>
         <li className="py-6 text-4xl">
-          <a href='/projects'>Projects</a>
+          <a href='/projects' style={{ color: location.pathname === '/projects' ? colors.Accent : colors.Text }}>Projects</a>
         </li>
         <li className="py-6 text-4xl">
-          <a href='/resume'>Resume</a>
+          <a href='/resume' style={{ color: location.pathname === '/resume' ? colors.Accent : colors.Text }}>Resume</a>
         </li>
         <li className="py-6 text-4xl">
-          <a href='/contact'>Contact</a>
+          <a href='/contact' style={{ color: location.pathname === '/contact' ? colors.Accent : colors.Text }}>Contact</a>
         </li>
       </ul>
     </div>
@@ -65,3 +66,4 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
