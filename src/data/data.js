da1c279be3a1fb64189f4491af58428f43200465
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-07-20 15:45:09 -0700

    updated projects

--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,21 @@
 import BringPopcorn from '../assets/bring-popcorrn-logo.png';
 import getFit from '../assets/fit-girl.png';
 import FarmersMarket from '../assets/farm-pic.png';
+import bklyn from '../assets/bklyn.png';
+import codingQuiz from '../assets/codingQuiz.png'
+import weatherDashboard from '../assets/weatherDashboard.png';
 
 export const data = [
-    {
+        {
         id:1,
+        name:"BKLYN",
+        description: "A MERN E-commerce app that allows users to buy and sell products in a user-friendly platform.",
+        image: bklyn,
+        github:"https://github.com/lgenzone/bklyn-shop",
+        live:"https://bklyn-shop-app-7862908fb146.herokuapp.com/",
+    },
+    {
+        id:2,
         name:"Bring Popcorn",
         description: "A movie search website that uses third-party APIs to provide data in one place, reducing the need for multiple apps.",
         image: BringPopcorn,
@@ -12,19 +23,35 @@ export const data = [
         live:"https://nbadraun.github.io/Bring-Popcorn/",
     },
     {
-        id:2,
+        id:3,
         name:"getFit",
         description: "A fitness app that uses MVC paradigm and various softwares to store and manipulate user exercise data, allowing users achieve their fitness goals.",
         image: getFit,
         github:"https://github.com/Jelenarog/Workout-planner",
-        live:"",
+        live: "https://getfit-fitness-tracker.herokuapp.com/",
     },
     {
-        id:3,
+        id:4,
         name:"Farmer's Market",
         description: "An online farmer's market that connects local producers with consumers to provide fresh produce through a user-friendly platform.",
         image: FarmersMarket,
         github:"https://github.com/lgenzone/Farmers-Market",
-        live:"https://farmermarket.herokuapp.com/",
-    }
+        live:"https://farm-market-shop.herokuapp.com/",
+    },
+    {
+        id:5,
+        name:"JavaScript Quiz",
+        description: "A timed coding quiz with multiple-choice questions that stores high scores.",
+        image: codingQuiz,
+        github:"https://github.com/lgenzone/code-quiz",
+        live:"https://lgenzone.github.io/code-quiz/",
+    },
+    {
+        id:6,
+        name:"Weather Dashboard",
+        description: "A weather dashboard that allows users to search for a city and view current and future weather conditions.",
+        image: weatherDashboard,
+        github:"https://github.com/lgenzone/five-day-weather-forecast",
+        live:"https://lgenzone.github.io/five-day-weather-forecast/",
+    },
 ]
